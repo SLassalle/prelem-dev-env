141e8e4895738df554840d12d327d4fecc4c37da
“[SLassalle]” <[lassallesebastien44@gmail.com]>
2025-06-16 21:45:52 +0200

    chore: ajout copie auto .env à partir de .env.example dans script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cp .env.example .env 2>/dev/null
+
+# Copie automatique du fichier .env s’il n’existe pas
+if [ ! -f .env ]; then
+  echo "Création du fichier .env depuis .env.example"
+  cp .env.example .env
+fi
+
+echo "Lancement de l'environnement Docker..."
 docker-compose up --build
